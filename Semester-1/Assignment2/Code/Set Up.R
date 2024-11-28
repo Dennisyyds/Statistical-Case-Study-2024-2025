9a70f0ecdbc8619ceed8bdbb1f13074d58cd7a2d
Dennisyyds <91064350+Dennisyyds@users.noreply.github.com>
2024-11-28 20:03:15 +0000

    Add files via upload

---
+++ Semester-1/Assignment2/Code/Set Up.R
@@ -0,0 +1,5 @@
+#Set Up
+source("~/Desktop/Stat Case study/Semester1/Assignment2/Assignment2 Code final version/stylometryfunctions.R")
+source("~/Desktop/Stat Case study/Semester1/Assignment2/Assignment2 Code final version/reducewords.R")
+GPTCorpus <- loadCorpus("~/Desktop/Stat Case study/Semester1/Assignment2/functionwords/functionwords/GPTfunctionwords/","functionwords")
+HumanCorpus <- loadCorpus("~/Desktop/Stat Case study/Semester1/Assignment2/functionwords/functionwords/humanfunctionwords/","functionwords")
