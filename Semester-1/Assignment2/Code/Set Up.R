bba1aa92fb56c5c36850ac3310f75f774ce96e60
Dennisyyds <91064350+Dennisyyds@users.noreply.github.com>
2024-11-29 11:17:27 +0000

    Delete Semester-1/Assignment2/Code directory

--- Semester-1/Assignment2/Code/Set Up.R
+++
@@ -1,5 +0,0 @@
-#Set Up
-source("~/Desktop/Stat Case study/Semester1/Assignment2/Assignment2 Code final version/stylometryfunctions.R")
-source("~/Desktop/Stat Case study/Semester1/Assignment2/Assignment2 Code final version/reducewords.R")
-GPTCorpus <- loadCorpus("~/Desktop/Stat Case study/Semester1/Assignment2/functionwords/functionwords/GPTfunctionwords/","functionwords")
-HumanCorpus <- loadCorpus("~/Desktop/Stat Case study/Semester1/Assignment2/functionwords/functionwords/humanfunctionwords/","functionwords")
